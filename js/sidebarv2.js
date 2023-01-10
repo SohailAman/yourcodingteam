e3b6368e20125df65169e0c7b7a9719f138d1037
SohailAman <101714964+SohailAman@users.noreply.github.com>
2023-01-10 22:16:17 +0500

    Update

--- js/sidebarv2.js
+++ js/sidebarv2.js
@@ -166,3 +166,20 @@ closeBtn.addEventListener('click', () => {
     menuSlide.classList.remove('slideInn');
     menuSlide.classList.add('slideOut');
 })
+
+
+//---scroll to-----
+$(function () {
+    $('.smoothScroll').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
