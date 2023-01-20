0f7586fa6da8fab4cb2e142fd799bde641e3afbb
SohailAman <101714964+SohailAman@users.noreply.github.com>
2023-01-20 18:54:04 +0500

    Form Updated

--- submit_form.php
+++ submit_form.php
@@ -1,15 +1,14 @@
 <?php
-// Replace with your email address
 $to = "sohailaman121@gmail.com";
 
 // Collect form data
-$first_name = $_GET['first_name'];
-$last_name = $_GET['last_name'];
-$company_name = $_GET['company_name'];
-$email = $_GET['email'];
-$country_code = $_GET['country_code'];
-$phone_number = $_GET['phone_number'];
-$developer = $_GET['developer'];
+$first_name = $_POST['First Name'];
+$last_name = $_POST['Last Name'];
+$company_name = $_POST['Company Name'];
+$email = $_POST['Email'];
+$country_code = $_POST['Country Code'];
+$phone_number = $_POST['Phone Number'];
+$developer = $_POST['developer'];
 
 // Compose email subject and body
 $subject = "New Form Submission";
